a160ea0fad7fb6f87859c97a0b37697e58d6f073
FADII143 <128960836+FADII143@users.noreply.github.com>
2023-04-23 10:26:17 +0500

    Create Fahad.py

---
+++ Fahad.py
@@ -0,0 +1,37 @@
+import os, platform
+
+ 
+
+try:
+
+ 
+
+    import requests
+
+ 
+
+except:
+
+ 
+
+    os.system('pip install requests')
+
+ 
+
+import requests
+
+ 
+
+bit = platform.architecture()[0]
+
+ 
+
+if bit == '64bit':
+
+ 
+
+    from marshal_enc import exit
+
+ 
+
+    exit()
